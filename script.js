95eadcefdc1f4f1f317ef7988c6a0a852c81723b
Harshna <hharshna.n@gmail.com>
2021-09-29 12:06:47 +0530

    feat: js file
    added features

---
+++ script.js
@@ -0,0 +1,26 @@
+const txtInput = document.querySelector("#txt-input");
+const btnTranslate = document.querySelector("#btn-translate");
+const divOutput = document.querySelector("#div-output");
+
+// "https://api.funtranslations.com/translate/draconic.json"
+var apiSource = "https://api.funtranslations.com/translate/dothraki.json";
+function constructUrl(input){
+    return apiSource + "?" + "text=" + input;
+}
+
+function errorHandler(error){
+    divOutput.innerText = "error occured. try again later";
+}
+
+function clickHandler(){
+    var input = txtInput.value;
+    var url = constructUrl(input);
+     fetch(url)
+    .then(response => response.json())
+    .then(json => {
+        divOutput.innerText = json.contents.translated
+    })
+    .catch(errorHandler)
+};
+
+btnTranslate.addEventListener("click", clickHandler)
